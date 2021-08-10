da5cb61855c5b102b40a89aec33d651bf25ce4d4
afrizzal <apratama8@gmail.com>
2021-08-10 21:48:01 +0700

    GET, POST, DEL method

--- routes/media.js
+++ routes/media.js
@@ -2,9 +2,28 @@ const express = require('express');
 const router = express.Router();
 const isBase64 = require('is-base64');
 const base64Img = require('base64-img');
+const fs = require('fs');
 
 const {Media} = require('../models')
 
+// Method GET
+router.get('/', async(req, res) => {
+  const media = await Media.findAll({
+    attributes: ['id', 'image']
+  });
+
+  const mappedMedia = media.map((m) => {
+    m.image = `${req.get('host')}/${m.image}`;
+    return m;
+  })
+
+  return res.json({
+    status: 'success',
+    data: mappedMedia
+  });
+});
+
+// Method POST
  router.post('/', (req, res) => {
    const image = req.body.image;
 
@@ -12,6 +31,7 @@ const {Media} = require('../models')
     return res.status(400).json({ status: 'error', message: 'invalid base64'});
   }
 
+    // Save Image to db
   base64Img.img(image, './public/images', Date.now(), async (err, filepath) => {
     if (err) {
       return res.status(400).json({ status: 'error', message: err. message });
@@ -19,16 +39,40 @@ const {Media} = require('../models')
 
     const filename = filepath.split("\\").pop().split("/").pop();
 
-    const media = await Media.create({ image: `image/${filename}` });
+    const media = await Media.create({ image: `images/${filename}` });
 
     return res.json ({
-      status: 'sucess',
+      status: 'success',
       data: {
         id: media.id,
         image: `${req.get('host')}/images/${filename}`
       }
     })
   })
+ });
+
+ // Method DELETE
+ router.delete('/:id', async (req, res) => {
+    const id = req.params.id;
+
+    const media = await Media.findByPk(id);
+
+    if (!media) {
+      return res.status(404).json({ status: 'error', message: 'media not found'});
+    }
+  
+    fs.unlink(`./public/${media.image}`, async (err) => {
+      if (err) {
+        return res.status(400).json({ status: 'error', message: err.message });
+      }
+
+      await media.destroy();
+
+      return res.json({
+        status: 'success',
+        message: 'image deleted'
+      });
+    });
  });
 
 module.exports = router;
